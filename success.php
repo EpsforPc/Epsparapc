94482687579028eab60df9ec784e05f2ed8b6d99
EpsforPc <maicolsuarez055@gmail.com>
2024-10-26 09:15:39 -0500

    Update success.php

--- success.php
+++ success.php
@@ -1,9 +1,1 @@
-<?php
-session_start();
 
-// Verificar si el usuario esta autenticado
-if (!isset($_SESSION['user'])) {
-    header("Location: index.php"); // Redirigir al usuario al inicio de sesi�n si no est� autenticado
-    exit();
-}
-?>
